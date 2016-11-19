39bf72dad7d6c0272d1db1ca86c6ecfd1706967d
jamiegood <jamiegood@gmail.com>
2016-11-19 17:44:36 +0000

    Added Auth provider

--- src/providers/auth.ts
+++ src/providers/auth.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 import { Storage } from '@ionic/storage';
@@ -12,34 +12,90 @@ var myconfig ={
 @Injectable()
 export class Auth {
 
+
+  public token: any;
+
   constructor(public http: Http, public storage: Storage) {
     console.log('Hello Auth Provider');
     console.log(myconfig);
   }
 
-  checkAuthentication() {
+  checkAuthentication(){
+
+    return new Promise((resolve, reject) => {
+
+        //Load token if exists
+        this.storage.get('token').then((value) => {
+
+            this.token = value;
+
+            let headers = new Headers();
+            headers.append('Authorization', this.token);
+
+            this.http.get('https://YOUR_HEROKU_APP.herokuapp.com/api/auth/protected', {headers: headers})
+                .subscribe(res => {
+                    resolve(res);
+                }, (err) => {
+                    reject(err);
+                });
+
+        });
+
+     });
 
   }
 
 
-  createAccount(account) {
+  createAccount(details){
 
-  }
+      return new Promise((resolve, reject) => {
 
-  login(user) {
+          let headers = new Headers();
+          headers.append('Content-Type', 'application/json');
+
+          this.http.post('https://YOUR_HEROKU_APP.herokuapp.com/api/auth/register', JSON.stringify(details), {headers: headers})
+            .subscribe(res => {
+
+              let data = res.json();
+              this.token = data.token;
+              this.storage.set('token', data.token);
+              resolve(data);
+
+            }, (err) => {
+              reject(err);
+            });
+
+      });
+
+    }
+
+      login(credentials){
+        
+        return new Promise((resolve, reject) => {
+
+            let headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+
+            this.http.post('https://YOUR_HEROKU_APP.herokuapp.com/api/auth/login', JSON.stringify(credentials), {headers: headers})
+              .subscribe(res => {
+
+                let data = res.json();
+                this.token = data.token;
+                this.storage.set('token', data.token);
+                resolve(data);
+
+                resolve(res.json());
+              }, (err) => {
+                reject(err);
+              });
+
+        });
+      }
 
 
-   return new Promise( (resolve, reject) => {
 
-
-     //this.http.post('')
-   });
-
-  }
-
-
-  logout(){
-
-  }
+      logout(){
+        this.storage.set('token', '');
+      }
 
 }
